2c6950b848cfcc240d7944698f9f839d459f12a2
Muhammad Waqas <124439937+muhammadwaqas964@users.noreply.github.com>
2024-11-24 20:51:04 +0500

    Update test_lambda_function.py

--- test/test_lambda_function.py
+++ test/test_lambda_function.py
@@ -1,15 +1,9 @@
 import json
-import boto3
 from unittest.mock import patch
+import boto3
 from moto import mock_dynamodb
 import sys
 import os
-
-# Mock AWS credentials for moto
-os.environ['AWS_ACCESS_KEY_ID'] = 'test'
-os.environ['AWS_SECRET_ACCESS_KEY'] = 'test'
-os.environ['AWS_SECURITY_TOKEN'] = 'test'
-os.environ['AWS_SESSION_TOKEN'] = 'test'
 
 # Add the path of the lambda_function module to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'MyProject')))
@@ -19,7 +13,6 @@ from lambda_function import lambda_handler  # Import using the correct path
 @mock_dynamodb
 def test_lambda_handler():
     # Set up mock DynamoDB
-    boto3.setup_default_session(region_name='us-east-1')  # Ensure the region is set correctly
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')  # Specify the region here
     table = dynamodb.create_table(
         TableName='waqasdynamodb',
